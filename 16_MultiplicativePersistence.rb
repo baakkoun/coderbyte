fe37d976397596a2385189192a3532b111313a89
baakkoun <baak.ko.un@gmail.com>
2015-07-31 13:41:17 +0900

    Create 16_MultiplicativePersistence.rb

---
+++ 16_MultiplicativePersistence.rb
@@ -0,0 +1,26 @@
+def MultiplicativePersistence(num)
+
+  # code goes here
+  
+  #if num > = 10
+  arr = Array.new
+  
+  loop do
+    if num >= 10
+       num = num.to_s.scan(/./).map {|e| e.to_i }
+       num = num.inject(:+)
+    else num < 10 
+      break
+    end
+    arr = arr.push(num)
+  end
+   
+
+    #  num = num.split('.')
+  return arr.count
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+MultiplicativePersistence(STDIN.gets) 
